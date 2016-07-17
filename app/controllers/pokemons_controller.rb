3e76dbeaae7c173f042dd92e22bc72f74a2cf1ed
Brad Johnson <cdale77@gmail.com>
2016-07-17 13:08:06 -0700

    default

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,8 +2,8 @@ class PokemonsController < ApplicationController
 
   def index
     pokemons = Pokemon.all.includes(:pokemon_type)
-    if stale? pokemons
+    #if stale? pokemons
       render json: pokemons
-    end
+    #end
   end
 end
